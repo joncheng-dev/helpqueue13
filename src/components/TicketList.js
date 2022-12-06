7f52e4bb2a588e115ea7057bf6838a494bbb9acd
Jonathan Cheng <jcheng461@gmail.com>
2022-12-06 23:22:44 +0900

    user can create new ticket to add to ticketList

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,29 +1,12 @@
 import React from "react";
 import Ticket from "./Ticket";
 
-function TicketList() {
-  const ticketList = [
-    {
-      names: "Homer and Bart",
-      location: "Quik-e-Mart",
-      issue: "out of beer",
-    },
-    {
-      names: "Marge and Lisa",
-      location: "Springfield Public Library",
-      issue: "can't find overdue book",
-    },
-    {
-      names: "Patty and Selma",
-      location: "Moe's Tavern",
-      issue: "out of smokes",
-    },
-  ];
+function TicketList(props) {
   return (
     <React.Fragment>
       <h2>Ticket List</h2>
       <hr />
-      {ticketList.map((ticket) => (
+      {props.ticketList.map((ticket) => (
         <Ticket names={ticket.names} location={ticket.location} issue={ticket.issue} />
       ))}
     </React.Fragment>
